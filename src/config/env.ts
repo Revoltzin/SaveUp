bd8d53e2b04a2eae0ddc66b3b1969af112337dca
Revoltzin <jat1963@gmail.com>
2026-08-02 12:14:47 -0300

    chore: env.ts setup to validate environment variables

---
+++ src/config/env.ts
@@ -0,0 +1,28 @@
+import "dotenv";
+
+interface Env {
+    JWT_SECRET: string;
+    DATABASE_URL: string;
+    PORT: number;
+    NODE_ENV: string;
+}
+
+function getEnvVar(key: string): string {
+    const value = process.env[key];
+
+    if (!value) {
+        console.error("[env] Environment variable required missing: ${key}");
+        process.exit(1);
+    }
+
+    return value;
+}
+
+const env: Env = {
+    JWT_SECRET: getEnvVar("JWT_SECRET"),
+    DATABASE_URL: getEnvVar("DATABASE_URL"),
+    PORT: Number(process.env.PORT) || 3000,
+    NODE_ENV: process.env.NODE_ENV ?? "development",
+};
+
+export default env;
