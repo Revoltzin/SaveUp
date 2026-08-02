b935ad4e41f6e203dd8f7498b625d2c80e1e8933
Revoltzin <jat1963@gmail.com>
2026-08-01 23:56:49 -0300

    fix: added type like(unknow) in errorHandler and fix lint no-explicit-any

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
-export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
+// Interace seria como o contrato do objeto e as tipages/regras
+// definidas somente para esse objeto
+interface AppError extends Error {
+    statusCode?: number;
+}
+
+export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
     console.log(err);
-    const status = err.statusCode ?? 500;
-    res.status(status).json({
-        message: err.message ?? "Internal Server Error!",
-    });
+    const status =
+        err instanceof Error && "statusCode" in err
+            ? ((err as AppError).statusCode ?? 500) // Retorna 500 somente se o statusCode for nulo ou undefined
+            : 500;
+    const message = err instanceof Error ? err.message : "Internal Server error";
+    res.status(status).json(message);
 }
