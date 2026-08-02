147649120d5c39a2fd040197903dbb0c4f1aaaa2
Revoltzin <jat1963@gmail.com>
2026-08-02 18:28:43 -0300

    fix: Added env import and simplify the method

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
-import { errorHandler }  from "./middlewares/errorHandler"
+import { errorHandler } from "./middlewares/errorHandler";
+import env from "./config/env";
 
-const app = express()
+const app = express();
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = env.PORT ?? 3000;
 app.listen(PORT, () => console.log("Server Online"));
