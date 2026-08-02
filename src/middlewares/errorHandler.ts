a363195c05a77c3462ac4add6dd06c34946c8a98
Revoltzin <jat1963@gmail.com>
2026-08-02 18:56:33 -0300

    refactor: Simplify error handling logic in errorHandler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,17 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-
-// Interace seria como o contrato do objeto e as tipages/regras
-// definidas somente para esse objeto
-interface AppError extends Error {
-    statusCode?: number;
-}
+import AppError from "../errors/AppError";
 
 export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
     console.log(err);
-    const status =
-        err instanceof Error && "statusCode" in err
-            ? ((err as AppError).statusCode ?? 500) // Retorna 500 somente se o statusCode for nulo ou undefined
-            : 500;
-    const message = err instanceof Error ? err.message : "Internal Server error";
-    res.status(status).json(message);
+    if (err instanceof AppError) return res.status(err.statusCode).json({ message: err.message });
+
+    res.status(500).json({ message: "Internal Server Error" });
 }
