2933b34dab1cc00211f25899e25f3f7dcf0a530e
Revoltzin <jat1963@gmail.com>
2026-08-02 19:41:39 -0300

    feat: Implement createUser function for user registration with error handling

---
+++ src/services/user.service.ts
@@ -0,0 +1,25 @@
+import { hashPassword } from "../utils/hash";
+import prisma from "../lib/prisma";
+import { Prisma } from "../generated/prisma/client";
+import AppError from "../errors/AppError";
+
+interface CreateUserInput {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export async function createUser({ name, email, password }: CreateUserInput) {
+    const hashedPassword = await hashPassword(password);
+
+    try {
+        const user = await prisma.user.create({
+            data: { name, email, password: hashedPassword },
+        });
+        return user;
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+            throw new AppError(409, "Email already registered");
+        }
+    }
+}
