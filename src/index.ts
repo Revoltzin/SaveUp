017dfd7e2b33aeba8cfd69e09145c01049715ec3
Revoltzin <jat1963@gmail.com>
2026-08-23 23:33:25 -0300

    chore: global Rate Limiter added

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { errorHandler } from "./middlewares/errorHandler";
 import env from "./config/env";
 import routes from "./routes/routes";
+import rateLimiterGlobal from "./middlewares/rateLimiterGlobal";
 
 const app = express();
 app.use(express.json());
+app.use(rateLimiterGlobal);
 
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
