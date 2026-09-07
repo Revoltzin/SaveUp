8a35eddad98ef506555637973d79f5c064e06441
Revoltzin <jat1963@gmail.com>
2026-09-07 00:47:15 -0300

    feat: add contribution routes to savings goal routes

--- src/routes/savingsGoal.routes.ts
+++ src/routes/savingsGoal.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { create, list, getById, update, remove } from "../controllers/savingsGoal.controller";
 import { authJwt } from "../middlewares/authMiddleware";
+import contributionRoutes from "./contribution.routes";
 
 const savingsGoalRoutes = Router();
 
@@ -9,5 +10,6 @@ savingsGoalRoutes.get("/", authJwt, list);
 savingsGoalRoutes.get("/:id", authJwt, getById);
 savingsGoalRoutes.patch("/:id", authJwt, update);
 savingsGoalRoutes.delete("/:id", authJwt, remove);
+savingsGoalRoutes.use("/:id/contributions", contributionRoutes);
 
 export default savingsGoalRoutes;
